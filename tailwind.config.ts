fec570c2f5c4f8762ff5b8df65e9b4e90fda307c
Roman Nenstiel <roman@coroyo.de>
2026-02-19 21:30:43 +0100

    add the right font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ export default {
         "2xl": "1400px",
       },
     },
+    fontFamily: {
+      sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+      heading: ["Montserrat", "ui-sans-serif", "system-ui", "sans-serif"],
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
